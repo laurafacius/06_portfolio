cde1c702b06213f82050b4344dc84491d30e399f
Laura <lafa0001@stud.kea.dk>
2023-06-07 09:43:12 +0200

    indhold

---
+++ menu.js
@@ -0,0 +1,10 @@
+/*******************************burger menu ******************* */
+
+var burgerMenu = document.getElementById("burger-menu");
+
+var overlay = document.getElementById("menu");
+
+burgerMenu.addEventListener("click", function () {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
