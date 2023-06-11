29960c0e25eef2b1b333ace540824fcbbc89545e
Laura <lafa0001@stud.kea.dk>
2023-06-11 17:14:17 +0200

    noter til js

--- menu.js
+++ menu.js
@@ -1,10 +1,15 @@
 /*******************************burger menu ******************* */
-
+// Få elementet med ID'et "burger-menu" og gem det i variablen burgerMenu
 var burgerMenu = document.getElementById("burger-menu");
 
+// Få elementet med ID'et "menu" og gem det i variablen overlay
 var overlay = document.getElementById("menu");
 
+// Tilføj en klik-hændelseslytter til burgerMenu-elementet
 burgerMenu.addEventListener("click", function () {
+  // Skift tilstanden for klassen "close" på det klikkede element (burgerMenu)
   this.classList.toggle("close");
+
+  // Skift tilstanden for klassen "overlay" på overlay-elementet
   overlay.classList.toggle("overlay");
 });
--- popup.js
+++ popup.js
@@ -1,50 +1,63 @@
 /********************* pop-up ************************* */
-document.querySelector("popup1").addEventListener("click", openPopup1);
+// Tilføj en klik-hændelseslytter til elementet med selektoren "popup" og kald funktionen openPopup, når der klikkes
+document.querySelector("popup").addEventListener("click", openPopup);
 
+// Funktionen openPopup1 åbner popup1 ved at ændre display-stilen til "block" på elementet med ID'et "popup1"
 function openPopup1() {
   document.getElementById("popup1").style.display = "block";
 }
 
+// Funktionen closePopup1 lukker popup1 ved at ændre display-stilen til "none" på elementet med ID'et "popup1"
 function closePopup1() {
   document.getElementById("popup1").style.display = "none";
 }
 
+// Funktionen openPopup2 åbner popup2 ved at ændre display-stilen til "block" på elementet med ID'et "popup2"
 function openPopup2() {
   document.getElementById("popup2").style.display = "block";
 }
 
+// Funktionen closePopup2 lukker popup2 ved at ændre display-stilen til "none" på elementet med ID'et "popup2"
 function closePopup2() {
   document.getElementById("popup2").style.display = "none";
 }
 
+// Funktionen openPopup3 åbner popup3 ved at ændre display-stilen til "block" på elementet med ID'et "popup3"
 function openPopup3() {
   document.getElementById("popup3").style.display = "block";
 }
 
+// Funktionen closePopup3 lukker popup3 ved at ændre display-stilen til "none" på elementet med ID'et "popup3"
 function closePopup3() {
   document.getElementById("popup3").style.display = "none";
 }
 
+// Funktionen openPopup4 åbner popup4 ved at ændre display-stilen til "block" på elementet med ID'et "popup4"
 function openPopup4() {
   document.getElementById("popup4").style.display = "block";
 }
 
+// Funktionen closePopup4 lukker popup4 ved at ændre display-stilen til "none" på elementet med ID'et "popup4"
 function closePopup4() {
   document.getElementById("popup4").style.display = "none";
 }
 
+// Funktionen openPopup5 åbner popup5 ved at ændre display-stilen til "block" på elementet med ID'et "popup5"
 function openPopup5() {
   document.getElementById("popup5").style.display = "block";
 }
 
+// Funktionen closePopup5 lukker popup5 ved at ændre display-stilen til "none" på elementet med ID'et "popup5"
 function closePopup5() {
   document.getElementById("popup5").style.display = "none";
 }
 
+// Funktionen openPopup6 åbner popup6 ved at ændre display-stilen til "block" på elementet med ID'et "popup6"
 function openPopup6() {
   document.getElementById("popup6").style.display = "block";
 }
 
+// Funktionen closePopup6 lukker popup6 ved at ændre display-stilen til "none" på elementet med ID'et "popup6"
 function closePopup6() {
   document.getElementById("popup6").style.display = "none";
 }
